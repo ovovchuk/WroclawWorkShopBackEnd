b997078e98770f7521471879b7e60e55d2dae795
ovovchuk <ovovchuk@softjourn.com>
2016-12-13 12:50:05 +0200

    Initial commit

---
+++ src/main/java/com/example/WokshopApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WokshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WokshopApplication.class, args);
+	}
+}
